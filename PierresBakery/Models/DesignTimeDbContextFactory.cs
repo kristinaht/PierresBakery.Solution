5aa84f4acc55358afd61b3ddef1cc231acf26d38
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-03-27 09:38:09 -0700

    add code to DesignTime and PierresBakeryContext files within Models folder

---
+++ PierresBakery/Models/DesignTimeDbContextFactory.cs
@@ -0,0 +1,27 @@
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace PierresBakery.Models
+{
+  public class PierresBakeryContextFactory : IDesignTimeDbContextFactory<PierresBakeryContext>
+  {
+
+    PierresBakeryContext IDesignTimeDbContextFactory<PierresBakeryContext>.CreateDbContext(string[] args)
+    {
+      IConfigurationRoot configuration = new ConfigurationBuilder()
+          .SetBasePath(Directory.GetCurrentDirectory())
+          .AddJsonFile("appsettings.json")
+          .Build();
+
+      var builder = new DbContextOptionsBuilder<PierresBakeryContext>();
+      var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+      builder.UseMySql(connectionString);
+
+      return new PierresBakeryContext(builder.Options);
+    }
+  }
+}
---
+++ PierresBakery/Models/PierresBakeryContext.cs
@@ -0,0 +1,16 @@
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace PierresBakery.Models
+{
+  public class PierresBakeryContext : DbContext
+  // : DbContext to be replaced with IdentityDbContext<ApplicationUser> when Authentication is added !!!
+  {
+    public virtual DbSet<Treat> Treats { get; set; }
+    // public DbSet<Flavor> Flavors { get; set; }
+    // public DbSet<TreatFlavor> TreatFlavor { get; set; }
+
+    public PierresBakeryContext(DbContextOptions options) : base(options) { }
+  }
+}
