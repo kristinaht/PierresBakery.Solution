458d211bbadc92cc3aa60d86d317b21bdcf6aff5
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-03-27 12:10:32 -0700

    include Create route in TreatsController and add Treats/Create view

--- PierresBakery/Controllers/TreatsController.cs
+++ PierresBakery/Controllers/TreatsController.cs
@@ -19,5 +19,18 @@ namespace PierresBakery
       List<Treat> model = _db.Treats.ToList();
       return View(model);
     }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Treat treat)
+    {
+      _db.Treats.Add(treat);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
