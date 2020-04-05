70bed63d22573b768f1074845fc1669c98c17062
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-04-05 15:51:37 -0700

    add delete routes to FlavorsController

--- PierresBakery/Controllers/FlavorsController.cs
+++ PierresBakery/Controllers/FlavorsController.cs
@@ -67,5 +67,20 @@ namespace PierresBakery.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    public ActionResult Delete(int id)
+    {
+      var thisFlavor = _db.Flavors.FirstOrDefault(flavors => flavors.FlavorId == id);
+      return View(thisFlavor);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisFlavor = _db.Flavors.FirstOrDefault(flavors => flavors.FlavorId == id);
+      _db.Flavors.Remove(thisFlavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
